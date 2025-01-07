b0e0e3f67251e8bded79ef10a2b5f4ee2af64087
Arman Singh <arman.singh@tricog.com>
2025-01-07 20:57:11 +0530

    deployed server on render

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,17 @@
-
+import { Pool } from 'pg';
 const process = require('process');
-import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.DATABASE_URL);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Set to false for self-signed certificates
+  },
+});
 
-
-export const query = async (text: string) => {
+export const query = async (text: string, params?: any[]) => {
   try {
-    const res = await sql(text);
-    return res
+    const res = await pool.query(text, params);
+    return res.rows;
   } catch (err) {
     console.error('Database query error:', err);
     throw err;
